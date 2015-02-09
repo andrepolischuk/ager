d8bcfe03afe0185811a77e2782be6a4db9ef4c38
andrepolischuk <andre.polischuk@gmail.com>
2015-02-09 15:14:52 +0300

    Add mocha test

---
+++ test/test.js
@@ -0,0 +1,32 @@
+var ager = require('../');
+var should = require('should');
+
+describe('ager', function() {
+
+  it('should return undefined for incorrect args', function() {
+    should(ager()).be.type('undefined');
+    should(ager(72)).be.type('undefined');
+    should(ager('asd')).be.type('undefined');
+  });
+
+  it('should return undefined for future date', function() {
+    should(ager(2050, 6, 15)).be.type('undefined');
+    should(ager(new Date(2050, 5, 15))).be.type('undefined');
+  });
+
+  it('should return undefined for incorrect date', function() {
+    should(ager(1975, 7, 52)).be.type('undefined');
+    should(ager(1976, 13, 21)).be.type('undefined');
+  });
+
+  it('should return number', function() {
+    ager(1980, 8, 11).should.be.a.Number;
+    ager(new Date(1980, 7, 11)).should.be.a.Number;
+  });
+
+  it('should return 31', function() {
+    ager(1984, 1, 5).should.equal(31);
+    ager(new Date(1984, 0, 5)).should.equal(31);
+  });
+
+});
