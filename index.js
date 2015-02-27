fa4e518f50db06e07ee56330767f7099d9924d1b
andrepolischuk <andre.polischuk@gmail.com>
2015-02-27 15:37:22 +0300

    Add array as date support

--- index.js
+++ index.js
@@ -2,48 +2,45 @@
 'use strict';
 
 /**
- * Module
- * @param  {Object|Number} year
- * @param  {Number} month
- * @param  {Number} day
- * @return {Number}
+ * Module dependencies
  */
 
-function ager(year, month, day) {
+var dts = require('dts');
 
-  var birthday = year && typeof year.getMonth === 'function' ?
-    birthday = year : (month && day) ?
-    new Date(+year, +month - 1, +day) : undefined;
-
-  if (!birthday || +month > 12) {
-    return;
-  }
-
-  if (month) {
-    birthday.setFullYear(+year);
-  }
-
-  day = +day || birthday.getDate();
-
-  var today = new Date();
-  var age = today.getFullYear() - birthday.getFullYear();
-  var monthDaysLength = new Date(birthday.getFullYear(),
-    birthday.getMonth() + 1, 0).getDate();
-
-  if (monthDaysLength < day || today < birthday) {
-    return;
-  }
-
-  age -= today.getMonth() < birthday.getMonth() ? 1 : 0;
-  age -= today.getMonth() === birthday.getMonth() &&
-    today.getDate() < birthday.getDate() ? 1 : 0;
-
-  return age;
-
+try {
+  var type = require('type');
+} catch (err) {
+  var type = require('component-type');
 }
 
 /**
- * Module exports
+ * Today
  */
 
-module.exports = ager;
+var today = dts();
+
+/**
+ * Expose age
+ * @param  {Date|Array|Number} year
+ * @param  {Number} month
+ * @param  {Number} day
+ * @return {Number}
+ * @api public
+ */
+
+module.exports = function(year, month, day) {
+
+  var birthday = type(year) === 'date' ?
+    year : dts(year, month, day);
+
+  if (!birthday || birthday > today) {
+    return;
+  }
+
+  var age = today.getFullYear() - birthday.getFullYear();
+  age -= today.getMonth() < birthday.getMonth() ? 1 : 0;
+  age -= today.getMonth() === birthday.getMonth() &&
+    today.getDate() < birthday.getDate() ? 1 : 0;
+  return age;
+
+};
--- test/test.js
+++ test/test.js
@@ -1,32 +1,74 @@
-var ager = require('../');
-var should = require('should');
 
-describe('ager', function() {
+var ager = require('..');
+var assert = require('assert');
+
+describe('ager()', function() {
 
   it('should return undefined for incorrect args', function() {
-    should(ager()).be.type('undefined');
-    should(ager(72)).be.type('undefined');
-    should(ager('asd')).be.type('undefined');
+    assert(ager() === undefined);
+    assert(ager(72) === undefined);
+    assert(ager('asd') === undefined);
   });
 
+});
+
+describe('ager(date)', function() {
+
   it('should return undefined for future date', function() {
-    should(ager(2050, 6, 15)).be.type('undefined');
-    should(ager(new Date(2050, 5, 15))).be.type('undefined');
+    assert(ager(new Date(2050, 5, 15)) === undefined);
+  });
+
+  it('should return number', function() {
+    var age = ager(new Date(1980, 7, 11));
+    assert(typeof age === 'number' && !isNaN(age));
+  });
+
+  it('should return 31', function() {
+    assert(ager(new Date(1984, 1, 5)) === 31);
+  });
+
+});
+
+describe('ager(array)', function() {
+
+  it('should return undefined for future date', function() {
+    assert(ager([2050, 6, 15]) === undefined);
   });
 
   it('should return undefined for incorrect date', function() {
-    should(ager(1975, 7, 52)).be.type('undefined');
-    should(ager(1976, 13, 21)).be.type('undefined');
+    assert(ager([1975, 7, 52]) === undefined);
+    assert(ager([1976, 13, 21]) === undefined);
   });
 
-  it('should return number for 1980.8.11', function() {
-    ager(1980, 8, 11).should.be.a.Number.and.not.be.NaN;
-    ager(new Date(1980, 7, 11)).should.be.a.Number.and.not.be.NaN;
+  it('should return number', function() {
+    var age = ager([1980, 8, 11]);
+    assert(typeof age === 'number' && !isNaN(age));
   });
 
-  it('should return 31 for 1984.1.5', function() {
-    ager(1984, 1, 5).should.equal(31);
-    ager(new Date(1984, 0, 5)).should.equal(31);
+  it('should return 31', function() {
+    assert(ager([1984, 2, 5]) === 31);
+  });
+
+});
+
+describe('ager(year, month, day)', function() {
+
+  it('should return undefined for future date', function() {
+    assert(ager(2050, 6, 15) === undefined);
+  });
+
+  it('should return undefined for incorrect date', function() {
+    assert(ager(1975, 7, 52) === undefined);
+    assert(ager(1976, 13, 21) === undefined);
+  });
+
+  it('should return number', function() {
+    var age = ager(1980, 8, 11);
+    assert(typeof age === 'number' && !isNaN(age));
+  });
+
+  it('should return 31', function() {
+    assert(ager(1984, 2, 5) === 31);
   });
 
 });
