00b7e1a87e69c98ab43dde26d4f8c9cb3f82ebc2
andrepolischuk <andre.polischuk@gmail.com>
2016-01-05 22:34:43 +0300

    Add ava instead of mocha

--- test.js
+++ test.js
@@ -1,38 +1,41 @@
 'use strict';
+var test = require('ava');
 var ager = require('./');
-var assert = require('assert');
 var year = (new Date()).getFullYear();
 
-it('should return undefined for incorrect args', function () {
-  assert(ager() === undefined);
-  assert(ager(72) === undefined);
-  assert(ager('asd') === undefined);
+test('return undefined for incorrect args', function (t) {
+  t.is(ager(), undefined);
+  t.is(ager(72), undefined);
+  t.is(ager('asd'), undefined);
 });
 
-it('should return undefined for incorrect date', function () {
-  assert(ager([1975, 7, 52]) === undefined);
-  assert(ager([1976, 13, 21]) === undefined);
-  assert(ager(1975, 7, 52) === undefined);
-  assert(ager(1976, 13, 21) === undefined);
+test('return undefined for incorrect date', function (t) {
+  t.is(ager([1975, 7, 52]), undefined);
+  t.is(ager([1976, 13, 21]), undefined);
+  t.is(ager(1975, 7, 52), undefined);
+  t.is(ager(1976, 13, 21), undefined);
 });
 
-it('should return undefined for future date', function () {
-  assert(ager(new Date(year + 50, 5, 15)) === undefined);
-  assert(ager([year + 50, 6, 15]) === undefined);
-  assert(ager(year + 50, 6, 15) === undefined);
+test('return undefined for future date', function (t) {
+  t.is(ager(new Date(year + 50, 5, 15)), undefined);
+  t.is(ager([year + 50, 6, 15]), undefined);
+  t.is(ager(year + 50, 6, 15), undefined);
 });
 
-it('should return number', function () {
+test('return number', function (t) {
   var ageFromDate = ager(new Date(1980, 7, 11));
   var ageFromArray = ager([1980, 8, 11]);
   var ageFromArgs = ager(1980, 8, 11);
-  assert(typeof ageFromDate === 'number' && !isNaN(ageFromDate));
-  assert(typeof ageFromArray === 'number' && !isNaN(ageFromArray));
-  assert(typeof ageFromArgs === 'number' && !isNaN(ageFromArgs));
+  t.is(typeof ageFromDate, 'number');
+  t.is(typeof ageFromArray, 'number');
+  t.is(typeof ageFromArgs, 'number');
+  t.false(isNaN(ageFromDate));
+  t.false(isNaN(ageFromArray));
+  t.false(isNaN(ageFromArgs));
 });
 
-it('should return 31', function () {
-  assert(ager(new Date(year - 31, 0, 1)) === 31);
-  assert(ager([year - 31, 1, 1]) === 31);
-  assert(ager(year - 31, 1, 1) === 31);
+test('return 31', function (t) {
+  t.is(ager(new Date(year - 31, 0, 1)), 31);
+  t.is(ager([year - 31, 1, 1]), 31);
+  t.is(ager(year - 31, 1, 1), 31);
 });
