35e7cbaa85d87cea5a58132fa3b671e5e794a309
andrepolischuk <andre.polischuk@gmail.com>
2016-01-06 21:53:57 +0300

    ES2015ify

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
-'use strict';
-var type = require('component-type');
-var today = new Date();
+import type from 'component-type';
+const today = new Date();
 
-module.exports = function (year, month, day) {
-  var birthday;
+export default function (year, month, day) {
+  let birthday;
   if (type(year) === 'date') birthday = year;
-  if (type(year) === 'array') birthday = parse.apply(null, year);
+  if (type(year) === 'array') birthday = parse(...year);
   if (type(year) === 'number') birthday = parse(year, month, day);
   if (!birthday || birthday > today) return;
-  var age = today.getFullYear() - birthday.getFullYear();
+  let age = today.getFullYear() - birthday.getFullYear();
   age -= today.getMonth() < birthday.getMonth() ? 1 : 0;
-  age -= today.getMonth() === birthday.getMonth() &&
-    today.getDate() < birthday.getDate() ? 1 : 0;
+  age -= today.getMonth() === birthday.getMonth() && today.getDate() < birthday.getDate() ? 1 : 0;
   return age;
 };
 
 function parse(year, month, day) {
   if (!month || month > 12) return;
   if (!day || day > new Date(year, month, 0).getDate()) return;
-  var date = new Date(year, month - 1, day);
+  let date = new Date(year, month - 1, day);
   date.setFullYear(year);
   return date;
 }
--- test.js
+++ test.js
@@ -1,31 +1,30 @@
-'use strict';
-var test = require('ava');
-var ager = require('./');
-var year = (new Date()).getFullYear();
+import test from 'ava';
+import ager from './index.es5';
+const year = (new Date()).getFullYear();
 
-test('return undefined for incorrect args', function (t) {
+test('return undefined for incorrect args', t => {
   t.is(ager(), undefined);
   t.is(ager(72), undefined);
   t.is(ager('asd'), undefined);
 });
 
-test('return undefined for incorrect date', function (t) {
+test('return undefined for incorrect date', t => {
   t.is(ager([1975, 7, 52]), undefined);
   t.is(ager([1976, 13, 21]), undefined);
   t.is(ager(1975, 7, 52), undefined);
   t.is(ager(1976, 13, 21), undefined);
 });
 
-test('return undefined for future date', function (t) {
+test('return undefined for future date', t => {
   t.is(ager(new Date(year + 50, 5, 15)), undefined);
   t.is(ager([year + 50, 6, 15]), undefined);
   t.is(ager(year + 50, 6, 15), undefined);
 });
 
-test('return number', function (t) {
-  var ageFromDate = ager(new Date(1980, 7, 11));
-  var ageFromArray = ager([1980, 8, 11]);
-  var ageFromArgs = ager(1980, 8, 11);
+test('return number', t => {
+  const ageFromDate = ager(new Date(1980, 7, 11));
+  const ageFromArray = ager([1980, 8, 11]);
+  const ageFromArgs = ager(1980, 8, 11);
   t.is(typeof ageFromDate, 'number');
   t.is(typeof ageFromArray, 'number');
   t.is(typeof ageFromArgs, 'number');
@@ -34,7 +33,7 @@ test('return number', function (t) {
   t.false(isNaN(ageFromArgs));
 });
 
-test('return 31', function (t) {
+test('return 31', t => {
   t.is(ager(new Date(year - 31, 0, 1)), 31);
   t.is(ager([year - 31, 1, 1]), 31);
   t.is(ager(year - 31, 1, 1), 31);
